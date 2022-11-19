1401161db786e3b9fdc88508431cde422e0a8484
Kossy3 <ykoshi873@gmail.com>
2022-11-19 14:31:35 +0900

    Updates

---
+++ precache-manifest.3d5bf4a789f9e10da217bc36c6b956c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf0786f513dc8d1891db",
+    "url": "/react-practice-6x6reversi/static/css/main.4023300c.chunk.css"
+  },
+  {
+    "revision": "bf0786f513dc8d1891db",
+    "url": "/react-practice-6x6reversi/static/js/main.bf0786f5.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/react-practice-6x6reversi/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "60716cf0d140c934304b",
+    "url": "/react-practice-6x6reversi/static/js/2.60716cf0.chunk.js"
+  },
+  {
+    "revision": "ff06eecba25da7db5e48",
+    "url": "/react-practice-6x6reversi/static/js/runtime~main.ff06eecb.js"
+  },
+  {
+    "revision": "40e762ec2e8a27c2cc8daf46f359b93e",
+    "url": "/react-practice-6x6reversi/index.html"
+  }
+];
